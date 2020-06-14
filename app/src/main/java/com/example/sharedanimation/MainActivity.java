d2a631062fd004ad6bdd639cc49f2d9e8ba984b3
Deepak681 <47095705+Deepak681@users.noreply.github.com>
2020-06-14 15:58:57 +0530

    Some Changes

--- app/src/main/java/com/example/sharedanimation/MainActivity.java
+++ app/src/main/java/com/example/sharedanimation/MainActivity.java
@@ -1,11 +1,13 @@
 package com.example.sharedanimation;
-
 import android.app.ActivityOptions;
 import android.content.Intent;
 import android.os.Build;
+
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatDelegate;
 
 import android.util.Pair;
@@ -14,41 +16,33 @@ import android.widget.ImageView;
 import android.widget.Switch;
 
 import androidx.cardview.widget.CardView;
-
 public class MainActivity extends AppCompatActivity {
+
     ImageView imageView;
     CardView cardView;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        imageView = findViewById(R.id.imageView);
-        cardView = findViewById(R.id.cardView);
-
-
-        cardView.setOnClickListener(new View.OnClickListener() {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_main );
+        imageView = findViewById( R.id.imageView );
+        cardView = findViewById( R.id.cardView );
+        cardView.setOnClickListener( new View.OnClickListener() {
             @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
             @Override
             public void onClick(View v) {
-
-                Intent intent = new Intent(MainActivity.this, Main2Activity.class);
-
+                Intent intent = new Intent( MainActivity.this, Main2Activity.class );
                 Pair[] pairs = new Pair[1];
-                pairs[0] = new Pair<View, String>(imageView, "img");
-
+                pairs[0] = new Pair<View, String>( imageView, "img" );
                 ActivityOptions options = null;
                 if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
-                    options = ActivityOptions.makeSceneTransitionAnimation(MainActivity.this, pairs);
+                    options = ActivityOptions.makeSceneTransitionAnimation( MainActivity.this, pairs );
                 }
                 assert options != null;
-                startActivity(intent, options.toBundle());
+                startActivity( intent, options.toBundle() );
             }
 
-
-        });
-
+        } );
 
     }
 }
